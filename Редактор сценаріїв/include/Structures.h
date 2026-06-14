054022d23a2c27d999551ff41c5e8f01ce11ac44
Nazar <nazarx9@gmail.com>
2026-06-14 18:14:37 +0200

    docs: add comment to explain structure's variable

--- Редактор сценаріїв/include/Structures.h
+++ Редактор сценаріїв/include/Structures.h
@@ -16,5 +16,6 @@ struct ParadoxGameData
 	QHash<QRgb, int> color_to_province;
 	QHash<QString, QRgb> countries_color;
 	QHash<QString, QList<int>> countries_provinces;
+	// Non-sea provinces, because they cannot be changed and they have no information.
 	QList<int> choosable_provinces;
 };
